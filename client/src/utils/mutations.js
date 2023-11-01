efcd8be3b99ba38435286e7a6f37653de1b41a49
James Brainard <james.brainard2@aol.com>
2023-11-01 14:14:58 -0700

    committing to pull Vanna's updates, I started queries for user, users, tasks and goals

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
 import { gql } from '@apollo/client';
-// ADD_TASK
-// ADD_GOAL
+// ADD_TASK 
+// ADD_GOAL- Takes in tasks to complete the goal ONLY IF user wants to.
 // 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,68 @@
 import { gql } from '@apollo/client';
-// Query_User
+// Query_User - with goals and tasks 
 // query_Goals(with tasks?)
 // QUERY_TASKS
 
-export const QUERY_USER = gql`
+export const QUERY_ALL_USER = gql`
   {
     user {
-      
+      _id
+      firstName
+      lastName
+      goals {
+        _id
+        title
+        description
+        completionDate
+        createdAt
+        completed
+        tasks {
+          _id
+          title
+          description
+          completionDate
+          priority
+          completed
+        }
+      }
     }
   }
+`;
+
+export const QUERY_TASKS = gql`
+  {
+    tasks {
+      _id
+      title
+      description
+      completionDate
+      priority
+      completed
+    }
+  }
+`;
+
+export const QUERY_GOALS = gql`
+  {
+    goals {
+      _id
+      title
+      description
+      completionDate
+      createdAt
+      completed
+      tasks {
+        _id
+        title
+        description
+        completionDate
+        priority
+        completed
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  
 `
