ab1eb387a9fc9a9de9c70dac519c50dfc4317d65
James Brainard <james.brainard2@aol.com>
2023-11-01 15:00:39 -0700

    began working on /client/src/utils/mutations.js also added queries.js in same utils folder

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 // query_Goals(with tasks?)
 // QUERY_TASKS
 
-export const QUERY_ALL_USER = gql`
+export const QUERY_ALL_USERS = gql`
   {
     user {
       _id
@@ -29,7 +29,7 @@ export const QUERY_ALL_USER = gql`
   }
 `;
 
-export const QUERY_TASKS = gql`
+export const QUERY_ALL_TASKS = gql`
   {
     tasks {
       _id
@@ -38,12 +38,77 @@ export const QUERY_TASKS = gql`
       completionDate
       priority
       completed
+      user {
+        _id
+        firstName
+        lastName
+      }
+      goals {
+        _id
+        title
+        description
+        completionDate
+        createdAt
+        completed
+      }
     }
   }
 `;
 
-export const QUERY_GOALS = gql`
+export const QUERY_SINGLE_TASK = gql`
+  query getTask($taskId: ID!) {
+    tasks {
+      _id
+      title
+      description
+      completionDate
+      priority
+      completed
+    }
+    user {
+      _id
+      firstName
+      lastName
+    }
+    goals {
+      _id
+      title
+      description
+      completionDate
+      createdAt
+      completed
+    }
+  }
+`
+
+export const QUERY_ALL_GOALS = gql`
   {
+    goals {
+      _id
+      title
+      description
+      completionDate
+      createdAt
+      completed
+      tasks {
+        _id
+        title
+        description
+        completionDate
+        priority
+        completed
+      }
+      user {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const QUERY_SINGLE_GOAL = gql`
+  query getGoal($goalId: ID!) {
     goals {
       _id
       title
@@ -61,8 +126,30 @@ export const QUERY_GOALS = gql`
       }
     }
   }
-`;
+`
 
 export const QUERY_USER = gql`
-  
+  query getUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      firstName
+      lastName
+      goals {
+        _id
+        title
+        description
+        completionDate
+        createdAt
+        completed
+        tasks {
+          _id
+          title
+          description
+          completionDate
+          priority
+          completed
+        }
+      }
+    }
+  }
 `
