8bc6da67e59a57ba5ae94b5625642a5fe12df267
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-11-02 14:33:45 -0700

    updated app.jsx and tasks.jsx, safe revert point

--- client/src/pages/Tasks/Tasks.jsx
+++ client/src/pages/Tasks/Tasks.jsx
@@ -1,10 +1,10 @@
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import TasksList from '../../components/TaskComponents/TasksList';
-import PowerList from '../../components/TaskComponents/PowerList';
+// import TasksList from '../../components/TaskComponents/TasksList';
+// import PowerList from '../../components/TaskComponents/PowerList';
 
-import Auth from '../../utils/auth';
+// import Auth from '../../utils/auth';
 
 
 
