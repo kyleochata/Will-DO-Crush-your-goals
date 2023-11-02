8eabb0d55bfc43d9aaec5c5e411ab385f27a8d2a
ultimated1228 <stevenlucasmeyer@gmail.com>
2023-11-02 14:35:10 -0600

    auth0 throwing errors committing for more eyes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,10 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import { Outlet } from "react-router-dom";
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import LoginButton from "./components/loginButton/loginButton"
+import LogoutButton from "./components/logoutButton/logoutButton"
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -38,11 +40,13 @@ function App() {
 	return (
 		<ApolloProvider client={client}>
 			<div className="flex-column justify-flex-start min-100-vh">
-				<Header />
+				{/* <Header /> */}
 				<div className="container">
 					<Outlet />
+          <LoginButton />
+          <LogoutButton />
 				</div>
-				<Footer />
+				{/* <Footer /> */}
 			</div>
 		</ApolloProvider>
 	);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,50 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createbrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-import Error from './pages/Error.jsx';
-import Goals from './pages/Goals.jsx';
-import Tasks from './pages/Tasks.jsx';
-import Calendar from './components/calendar/calendar.jsx';
-import OneGoal from './pages/OneGoal.jsx';
-import OneTask from './pages/OneTask.jsx';
-import Home from './pages/Home.jsx';
-import Dashboard from './pages/Dashboard.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App.jsx";
+import "./index.css";
+import Error from "./pages/Error/Error.jsx";
+import Goals from "./pages/Goals/All-Goals.jsx";
+import Tasks from "./pages/Tasks/Tasks.jsx";
+import Calendar from "./components/calendar/calendar.jsx";
+import OneGoal from "./pages/Goals/One-Goal.jsx";
+// import OneTask from './pages/OneTask.jsx';
+import Home from "./pages/Home/Home.jsx";
+// import Dashboard from './pages/Dashboard.jsx';
 
-const router = createbrowserRouter([
+const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.REACT_APP_AUTH0_CLIEN_ID;
+
+const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
+    path: "/",
+    element: (
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
+    ),
     error: <Error />,
     children: [
       {
         index: true,
-        element: <Home />
-      }, {
-        path: '/dashboard',
-        element: <Dashboard />
-      }, {
-        path: '/goals',
+        element: <Home />,
+        // }, {
+        //   path: '/dashboard',
+        //   element: <Dashboard />
+      },
+      {
+        path: "/goals",
         element: <Goals />,
-      }, {
-        path: '/goals/:goalId',
+      },
+      {
+        path: "/goals/:goalId",
         element: <OneGoal />,
-      }, {
-        path: '/tasks',
+      },
+      {
+        path: "/tasks",
         element: <Tasks />,
-      }, {
-        path: '/tasks/:taskId',
-        element: <OneTask />,
-      }, {
-        path: '/calendar',
+        // }, {
+        // path: '/tasks/:taskId',
+        // element: <OneTask />,
+      },
+      {
+        path: "/calendar",
         element: <Calendar />,
-      }
-    ]
-  }
-])
+      },
+    ],
+  },
+]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
