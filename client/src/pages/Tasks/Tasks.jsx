b39593068807718fc7ed54e3f7f70a9a69fa2ad0
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-11-02 19:02:17 -0700

    updated task page and components

--- client/src/pages/Tasks/Tasks.jsx
+++ client/src/pages/Tasks/Tasks.jsx
@@ -1,16 +1,33 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-// import TasksList from '../../components/TaskComponents/TasksList';
+import TasksList from '../../components/TaskComponents/TasksList.jsx';
 // import PowerList from '../../components/TaskComponents/PowerList';
 
 // import Auth from '../../utils/auth';
 
-
+import { QUERY_USER } from '../../utils/queries';
 
 function Tasks() {
+  const { userId } = useParams();
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { userId: userId }
+  });
+
+  const user = data?.user || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>Tasks</div>
-  )
+		<div>
+      All Tasks
+      {/* need to add functionality to button, bring up modal when clicked */}
+      <button className='add-task-btn'>+ Add Task</button>
+      power list here
+      <TasksList tasks={user.tasks} />
+		</div>
+	);
 }
 export default Tasks
