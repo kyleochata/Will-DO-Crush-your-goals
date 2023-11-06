9568b61223485880af9dd9337b17d1f8da136421
kyleochata <kyleochata@gmail.com>
2023-11-06 10:50:38 -0800

    save for second AD pull

--- client/src/components/Goals-Component/Single-Goal.jsx
+++ client/src/components/Goals-Component/Single-Goal.jsx
@@ -1,74 +1,72 @@
-import { from, useMutation } from "@apollo/client";
-import { DELETE_GOAL, EDIT_GOAL,ADD_MEASURABLE } from '../../utils/mutations';
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import format_date from "../../utils/helpers";
+import { from, useMutation } from '@apollo/client'
+import { DELETE_GOAL, EDIT_GOAL, ADD_MEASURABLE } from '../../utils/mutations'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import format_date from '../../utils/helpers'
 import EditGoalBtn from '../../components/Goals-Component/EditGoalBtn'
-import "../Dashboard/Dashboard.css";
-import style from "../../pages/Tasks/Tasks.module.css"
+import '../Dashboard/Dashboard.css'
+import style from '../../pages/Tasks/Tasks.module.css'
 
 const format_date2 = (timestamp) => {
   //month is index 0-11. must add 1 to get correct month
-  let timeStamp = new Date(parseInt(timestamp));
-  let monthNum = timeStamp.getMonth();
+  let timeStamp = new Date(parseInt(timestamp))
+  let monthNum = timeStamp.getMonth()
   const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let currentMonth = months[monthNum];
-  let day = timeStamp.getDate();
-  let year = timeStamp.getFullYear();
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+  let currentMonth = months[monthNum]
+  let day = timeStamp.getDate()
+  let year = timeStamp.getFullYear()
 
-  return `${currentMonth} ${day}, ${year}`;
-};
+  return `${currentMonth} ${day} ${year}`
+}
 
 const SingleGoal = ({ filteredGoals }) => {
-  const [deleteGoal] = useMutation(DELETE_GOAL);
+  const [deleteGoal] = useMutation(DELETE_GOAL)
   // const [goalState, setGoalState] = useState(true);
-  const { goalId } = useParams();
-  const goal = filteredGoals[0];
-  console.log(goalId);
-  console.log(goal.completionDate);
-  const [editGoal, setEditGoal] = useState(false);
-  console.log(filteredGoals);
-  const [addMeasurable] = useMutation(ADD_MEASURABLE);
+  const { goalId } = useParams()
+  const goal = filteredGoals[0]
+  console.log(goalId)
+  console.log(goal.completionDate)
+  const [editGoal, setEditGoal] = useState(false)
+  console.log(filteredGoals)
+  const [addMeasurable] = useMutation(ADD_MEASURABLE)
   const handleDelClick = async () => {
     try {
       await deleteGoal({
         variables: { goalId: goalId },
-      });
-      window.location.replace("/goals");
+      })
+      window.location.replace('/goals')
     } catch (err) {
-      console.log("catching error");
-      throw err;
+      console.log('catching error')
+      throw err
     }
-  };
+  }
 
-  const [goalData, setGoalData] = useState(goal);
-
+  const [goalData, setGoalData] = useState(goal)
 
   useEffect(() => {
     if (!goalData.measurables) {
       setGoalData((prevGoalData) => ({
         ...prevGoalData,
-        measurables: []
-      }));
+        measurables: [],
+      }))
     }
-  }, [goalData, setGoalData]);
+  }, [goalData, setGoalData])
 
-  console.log("here");
-  console.log(goalData);
-
+  console.log('here')
+  console.log(goalData)
 
   // const handleInputChange = (event) => {
   //   setGoalData({
@@ -79,73 +77,76 @@ const SingleGoal = ({ filteredGoals }) => {
   // };
 
   const handleInputChange = (event) => {
-    const { name, value, options } = event.target;
-
+    const { name, value, options } = event.target
+    // if (name === 'completionDate') {
+    //   let date = new Date(value)
+    //   setGoalData((prevState) => ({
+    //     ...prevState,
+    //     [name]: date,
+    //   }))
+    // }
     if (event.target.multiple) {
       const selectedValues = Array.from(options)
-        .filter(option => option.selected)
-        .map(option => option.value);
-      setGoalData(prevState => ({
+        .filter((option) => option.selected)
+        .map((option) => option.value)
+      setGoalData((prevState) => ({
         ...prevState,
         [name]: selectedValues,
-      }));
+      }))
     } else {
-      setGoalData(prevState => ({
+      setGoalData((prevState) => ({
         ...prevState,
         [name]: value,
-      }));
+      }))
     }
-  };
+  }
 
-
-
-  const [changeGoal] = useMutation(EDIT_GOAL);
+  const [changeGoal] = useMutation(EDIT_GOAL)
   const updatedGoal = (goalData) => {
     changeGoal({ variables: goalData })
       .then((response) => {
-        console.log("Goal updated:", response.data.changeGoal);
+        console.log('Goal updated:', response.data.changeGoal)
       })
       .catch((error) => {
-        console.error("Error updating goal:", error);
-      });
-  };
+        console.error('Error updating goal:', error)
+      })
+  }
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    goalData.goalId = goalData._id;
-    updatedGoal(goalData);
-    setEditGoal(!editGoal);
-  };
+    event.preventDefault()
+    goalData.goalId = goalData._id
+    updatedGoal(goalData)
+    setEditGoal(!editGoal)
+  }
 
   //asdasdasdsadab
 
-
   const addMeasurableClick = async (event) => {
-    event.preventDefault();
-    const measurableToAdd = prompt("Enter new measurable:"); // Simple prompt for demo, consider a better UI for production
+    event.preventDefault()
+    const measurableToAdd = prompt('Enter new measurable:') // Simple prompt for demo, consider a better UI for production
     if (measurableToAdd) {
       try {
         const { data } = await addMeasurable({
           variables: {
             goalId: goalData._id,
-            title: measurableToAdd
-          }
-        });
+            title: measurableToAdd,
+          },
+        })
         if (data) {
-          setGoalData(prevState => ({
+          setGoalData((prevState) => ({
             ...prevState,
             measureables: [...prevState.measureables, measurableToAdd],
-          }));
+          }))
         }
       } catch (err) {
-        console.error("Error adding measurable", err);
+        console.error('Error adding measurable', err)
       }
     }
-  };
+  }
 
   const editGoalClick = () => {
     // const [goalData, setGoalData] = useState({});
-    setEditGoal(!editGoal);
-  };
+    setEditGoal(!editGoal)
+  }
 
   return (
     <div>
@@ -156,32 +157,40 @@ const SingleGoal = ({ filteredGoals }) => {
               <div className="cardText textAlign">
                 <h2 className="singlePageTitle">{goalData.title}</h2>
                 <div className="dashButtonContainer">
-                  <button onClick={editGoalClick} className="dashButton">Edit Goal</button>
-                  <button className="dashButton" >Complete</button>
-                  <button onClick={handleDelClick} className="dashButton">Delete</button>
+                  <button onClick={editGoalClick} className="dashButton">
+                    Edit Goal
+                  </button>
+                  <button className="dashButton">Complete</button>
+                  <button onClick={handleDelClick} className="dashButton">
+                    Delete
+                  </button>
                 </div>
                 <div className="goalDashSpacing"></div>
                 <div className="liItem">
                   <h3 className="subHeader">Description</h3>
-                <p className="singlePageText">  {goalData.description}</p>
+                  <p className="singlePageText"> {goalData.description}</p>
                 </div>
                 <div className="liItem">
-                <h3 className="subHeader">Your Why</h3>
-                <p className="regularText"> {goalData.why}</p>
+                  <h3 className="subHeader">Your Why</h3>
+                  <p className="regularText"> {goalData.why}</p>
                 </div>
                 <div className="liItem">
-                <h3 className="subHeader">Measurables</h3>
-                <p className="regularText"> {goalData.measurables}</p>
+                  <h3 className="subHeader">Measurables</h3>
+                  <p className="regularText"> {goalData.measurables}</p>
                 </div>
                 <div className="liItem">
-                <h3 className="subHeader">Target Date</h3>
-                <p className="regularText">{format_date2(goalData.completionDate)}</p>
+                  <h3 className="subHeader">Target Date</h3>
+                  <p className="regularText">
+                    {format_date2(goalData.completionDate)}
+                  </p>
                 </div>
                 <div className="liItem">
-                <h3 className="subHeader">Linked Tasks</h3>
-                <p className="regularText">Map over the associated tasks here once we have that logic built out</p>
+                  <h3 className="subHeader">Linked Tasks</h3>
+                  <p className="regularText">
+                    Map over the associated tasks here once we have that logic
+                    built out
+                  </p>
                 </div>
-                
               </div>
             </article>
           </div>
@@ -196,83 +205,99 @@ const SingleGoal = ({ filteredGoals }) => {
       )}
       {editGoal && (
         <div className={style.editContent}>
-        <div className={style.editTitle}>EDIT GOAL</div>
-        <div className={style.formContainer}>
-          <form onSubmit={handleSubmit} className={style.addTaskForm}>
-            <div className={style.formInputs}>
-            <label className={style.addTaskModalTxt}>
-              Title:
-              <div className={style.goalDesc}>A broad overview of your goal. "Learn to develop web applications"</div>
-              <input
-                placeholder="Your Goal Title"
-                type="text"
-                name="title"
-                value={goalData.title}
-                onChange={handleInputChange}
-                className={style.addTaskModalInput}
-                required
-              />
-            </label>
-            <label className={style.addTaskModalTxt}>
-              Description:
-              <div className={style.goalDesc}>Be specific with your goal. "Become a full stack developer specializing in JS" </div>
-              <textarea
-                placeholder="Give some specifics about your goal here"
-                name="description"
-                value={goalData.description}
-                onChange={handleInputChange}
-                className={style.addTaskModalInput}
-                required
-              />
-            </label>
-            <label className={style.addTaskModalTxt}>
-              Why:
-              <div className={style.goalDesc}>Make it relevant. "Becoming a full-stack web developer aligns with my career aspirations and will open up job opportunities in a field that I'm passionate about."  </div>
-              <textarea
-                placeholder="What's your why?"
-                name="why"
-                value={goalData.why}
-                onChange={handleInputChange}
-                className={style.addTaskModalInput}
-                required
-              />
-              </label>
-            <label className={style.addTaskModalTxt}>
-              Measurables
-              <div className={style.goalDesc}>Give quantifiable examples of how you will know your goal is completed. "Complete a full stack web bootcamp, build a portfolio, contribute to at least 3 open source Githubs, build at least 5 MERN applications" </div>
-              <textarea
-              placeholder="What metrics will help you know your goal is complete?"
-                name="measureables"
-                value={goalData.measureables}
-                onChange={handleInputChange}
-                className={style.addTaskModalInput}
-                required
-              />
-            </label>
-            <label className={style.addTaskModalTxt}>
-              Due Date:
-              <div className={style.goalDesc}>Making your goals time-bound provides a sense of urgency and a clear deadline, motivating you to take action and track your progress </div>
-              <input
-                type="date"
-                name="completionDate"
-                value={format_date(goalData.completionDate)}
-                onChange={handleInputChange}
-                className={style.addTaskModalInput}
-                required
-              />
-            </label>
-            </div>
-            <div className={style.submitButtonContainer}>
-              <button
-                className={style.submitButton}
-                type="submit"
-              >
-                Edit Goal
-              </button>
-            </div>
-          </form>
+          <div className={style.editTitle}>EDIT GOAL</div>
+          <div className={style.formContainer}>
+            <form onSubmit={handleSubmit} className={style.addTaskForm}>
+              <div className={style.formInputs}>
+                <label className={style.addTaskModalTxt}>
+                  Title:
+                  <div className={style.goalDesc}>
+                    A broad overview of your goal. "Learn to develop web
+                    applications"
+                  </div>
+                  <input
+                    placeholder="Your Goal Title"
+                    type="text"
+                    name="title"
+                    value={goalData.title}
+                    onChange={handleInputChange}
+                    className={style.addTaskModalInput}
+                    required
+                  />
+                </label>
+                <label className={style.addTaskModalTxt}>
+                  Description:
+                  <div className={style.goalDesc}>
+                    Be specific with your goal. "Become a full stack developer
+                    specializing in JS"{' '}
+                  </div>
+                  <textarea
+                    placeholder="Give some specifics about your goal here"
+                    name="description"
+                    value={goalData.description}
+                    onChange={handleInputChange}
+                    className={style.addTaskModalInput}
+                    required
+                  />
+                </label>
+                <label className={style.addTaskModalTxt}>
+                  Why:
+                  <div className={style.goalDesc}>
+                    Make it relevant. "Becoming a full-stack web developer
+                    aligns with my career aspirations and will open up job
+                    opportunities in a field that I'm passionate about."{' '}
+                  </div>
+                  <textarea
+                    placeholder="What's your why?"
+                    name="why"
+                    value={goalData.why}
+                    onChange={handleInputChange}
+                    className={style.addTaskModalInput}
+                    required
+                  />
+                </label>
+                <label className={style.addTaskModalTxt}>
+                  Measurables
+                  <div className={style.goalDesc}>
+                    Give quantifiable examples of how you will know your goal is
+                    completed. "Complete a full stack web bootcamp, build a
+                    portfolio, contribute to at least 3 open source Githubs,
+                    build at least 5 MERN applications"{' '}
+                  </div>
+                  <textarea
+                    placeholder="What metrics will help you know your goal is complete?"
+                    name="measureables"
+                    value={goalData.measureables}
+                    onChange={handleInputChange}
+                    className={style.addTaskModalInput}
+                    required
+                  />
+                </label>
+                <label className={style.addTaskModalTxt}>
+                  Due Date:
+                  <div className={style.goalDesc}>
+                    Making your goals time-bound provides a sense of urgency and
+                    a clear deadline, motivating you to take action and track
+                    your progress{' '}
+                  </div>
+                  <input
+                    type="date"
+                    name="completionDate"
+                    value={format_date(goalData.completionDate)}
+                    onChange={handleInputChange}
+                    className={style.addTaskModalInput}
+                    required
+                  />
+                </label>
+              </div>
+              <div className={style.submitButtonContainer}>
+                <button className={style.submitButton} type="submit">
+                  Edit Goal
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
       )}
     </div>
   )
@@ -282,4 +307,4 @@ const SingleGoal = ({ filteredGoals }) => {
 // 	measureables: [],
 // };
 
-export default SingleGoal;
+export default SingleGoal
