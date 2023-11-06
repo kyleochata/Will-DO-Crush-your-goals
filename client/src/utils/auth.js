6ada5ba8aabe7bf74c787f8e2a4526608a62d4ab
GSwordGCil <ac983042820@gmail.com>
2023-11-06 10:20:40 -0800

    123

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { jwtDecode } from "jwt-decode";
 
 class AuthService {
@@ -6,15 +5,6 @@ class AuthService {
 		const token = this.getToken();
 		return jwtDecode(token);
 	}
-=======
-// Import everything under an alias
-import * as jwtDecode from 'jwt-decode';
-
-class AuthService {
-  getProfile() {
-    return jwtDecode.default(this.getToken());
-  }
->>>>>>> 7cb3ee0cb40b5cfbcd55159ce81e57ec029435d6
 
   loggedIn() {
     const token = this.getToken();
@@ -22,7 +12,6 @@ class AuthService {
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
-<<<<<<< HEAD
 	isTokenExpired(token) {
 		// Decode the token to get its expiration time that was set by the server
 		const decoded = jwtDecode(token);
@@ -34,19 +23,6 @@ class AuthService {
 		// If token hasn't passed its expiration time, return `false`
 		return false;
 	}
-=======
-  isTokenExpired(token) {
-    // Decode the token to get its expiration time that was set by the server
-    const decoded = jwtDecode.default(token);
-    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
-    if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem("id_token");
-      return true;
-    }
-    // If token hasn't passed its expiration time, return `false`
-    return false;
-  }
->>>>>>> 7cb3ee0cb40b5cfbcd55159ce81e57ec029435d6
 
   getToken() {
     return localStorage.getItem("id_token");
